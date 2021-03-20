175b439af60ba320e6a2bc3ea1da22e52200cfaa
f.sousa <ng.dev.sousa@outlook.com>
2021-03-20 17:03:11 -0300

    Add User facade

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,6 +1,9 @@
 defmodule Exlivery do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate
 
-  def hello do
-    :world
+  def start_agents do
+    UserAgent.start_link(nil)
   end
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
 end
---
+++ lib/users/create_or_update.ex
@@ -0,0 +1,13 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
+
+  def call(%{name: name, email: email, cpf: cpf, age: age, address: address}) do
+    name
+    |> User.build(email, cpf, age, address)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:error, _reason} = error), do: error
+end
